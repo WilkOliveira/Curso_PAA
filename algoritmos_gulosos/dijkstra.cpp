d6f4b6077bc2d7f9c3f3dcdf95ec971794bd3172
Wilk Oliveira <wos@ic.ufal.br>
2017-03-13 17:53:59 -0300

    Curso_PAA

---
+++ algoritmos_gulosos/dijkstra.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <list>
+#include <queue>
+#define INFINITO 999999999
+
+using namespace std;
+
+/**
+* Classe para manipular o grafo
+*/
+class Grafo
+{
+private:
+	int vertices; // número de vértices
+
+	list<pair<int, int> > * adj; // ponteiro para um array que representa as listas de adjacências
+
+public:
+
+	/**
+	* Costrutor
+	*/
+	Grafo(int vertices)
+	{
+		this->vertices = vertices; // atribui o número de vértices
+
+		/**
+			Cria as listas, onde cada lista é uma lista de pairs e cada pair é formado pelo vértice destino e o custo
+		*/
+		adj = new list<pair<int, int> >[vertices];
+	}
+
+	/**
+	* Adiciona uma aresta ao grafo de v1 à v2, onde v1 e v2 representam os vertices
+	*/
+	void adicionaAresta(int v1, int v2, int custo)
+	{
+		adj[v1].push_back(make_pair(v2, custo));
+	}
+
+	/**
+	* Implementa o algoritmos de Dijkstra de acordo com os slides
+	*/
+	int dijkstra(int orig, int dest)
+	{
+
+		int dist[vertices]; // vetor de distâncias
+
+		/**
+        * Vetor de visitados, impede que um vertice visitado seja visitado novamente (guloso)
+		*/
+		int visitados[vertices];
+
+		// fila de prioridades de pair (distancia, vértice)
+		priority_queue < pair<int, int>,
+					   vector<pair<int, int> >, greater<pair<int, int> > > pq;
+
+		// inicia o vetor de distâncias e visitados
+		for(int i = 0; i < vertices; i++)
+		{
+			dist[i] = INFINITO;
+			visitados[i] = false;
+		}
+
+		// a distância de orig para orig é 0
+		dist[orig] = 0;
+
+		// insere na fila
+		pq.push(make_pair(dist[orig], orig));
+
+		// loop do algoritmo
+		while(!pq.empty())
+		{
+			pair<int, int> p = pq.top(); // extrai o pair do topo
+			int u = p.second; // obtém o vértice do pair
+			pq.pop(); // remove da fila
+
+			// verifica se o vértice não foi expandido
+			if(visitados[u] == false)
+			{
+				// marca como visitado
+				visitados[u] = true;
+
+				list<pair<int, int> >::iterator it;
+
+				// percorre os vértices "v" adjacentes de "u"
+				for(it = adj[u].begin(); it != adj[u].end(); it++)
+				{
+					// obtém o vértice adjacente e o custo da aresta
+					int v = it->first;
+					int custo_aresta = it->second;
+
+					// relaxamento (u, v)
+					if(dist[v] > (dist[u] + custo_aresta))
+					{
+						// atualiza a distância de "v" e insere na fila
+						dist[v] = dist[u] + custo_aresta;
+						pq.push(make_pair(dist[v], v));
+					}
+				}
+			}
+		}
+
+		// retorna a distância mínima até o destino
+		return dist[dest];
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	Grafo g(5);
+
+	g.adicionaAresta(0, 1, 4);
+	g.adicionaAresta(0, 2, 2);
+	g.adicionaAresta(0, 3, 5);
+	g.adicionaAresta(1, 4, 1);
+	g.adicionaAresta(2, 1, 1);
+	g.adicionaAresta(2, 3, 2);
+	g.adicionaAresta(2, 4, 1);
+	g.adicionaAresta(3, 4, 1);
+
+	cout << g.dijkstra(0, 4) << endl;
+
+	return 0;
+}
