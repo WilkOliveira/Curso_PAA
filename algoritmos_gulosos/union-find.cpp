b2f50f1410b54b9b02e84fd743805de8afab854b
Wilk Oliveira <wos@ic.ufal.br>
2017-03-16 01:16:31 -0300

    Curso_PAA

--- algoritmos_gulosos/union-find.cpp
+++ algoritmos_gulosos/union-find.cpp
@@ -3,27 +3,35 @@
 #include <stdlib.h>
 using namespace std;
 
-
-// função que busca o subconjunto de um elemento "v"
-int buscar(int subset[], int v)
+/**
+* Função recursiva que busca o subconjunto de um determnado elemento "v" do grafo
+* Enquanto houver vertices, percorre uma aresta ligada a um novo vertice
+*/
+int busca(int subset[], int v)
 {
 	if(subset[v] == -1)
 		return v;
-	return buscar(subset, subset[v]);
+	return busca(subset, subset[v]);
 }
 
-// função para unir dois subconjuntos em um único conjunto
+/**
+* Função para unir dois subconjuntos em um único conjunto
+* Usa a função busca para identificar os subconjuntos de cada vertice e em seguida os une
+*/
 void unir(int subset[], int v1, int v2)
 {
-	int v1_set = buscar(subset, v1);
-	int v2_set = buscar(subset, v2);
+	int v1_set = busca(subset, v1);
+	int v2_set = busca(subset, v2);
 
 	subset[v1_set] = v2_set;
 }
 
-int tem_ciclo(int grafo[3][3])
+/**
+* Função que identifica a formações de ciclos no grafo (para encontrar a arvore geradora minima)
+*/
+int identificaCiclo(int grafo[3][3])
 {
-	int * subset = (int*)malloc(3 * sizeof(int));
+	int * subset = (int*)malloc(3 * sizeof(int)); // malloc do c
 
 	// inicializa todos os subconjuntos como conjuntos de um único elemento
 	memset(subset, -1, sizeof(int) * 3);
@@ -34,8 +42,8 @@ int tem_ciclo(int grafo[3][3])
 		{
 			if(grafo[i][j] == 1)
 			{
-				int v1 = buscar(subset, i);
-				int v2 = buscar(subset, j);
+				int v1 = busca(subset, i);
+				int v2 = busca(subset, j);
 
 				if(v1 == v2)
 					return 1;
@@ -50,16 +58,20 @@ int tem_ciclo(int grafo[3][3])
 
 int main(int argc, char *argv[])
 {
+	/**
+	* Grafo representado como uma matriz de adjacencias
+	* Grafo sem pesos
+	*/
 	int grafo[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
-	grafo[0][1] = 1;
-	grafo[1][0] = 1;
+	grafo[0][1] = 1; // vertice 1 esta ligada a vertice 2
+	grafo[1][0] = 1; // vertice 2 esta ligada a vertice 1
 	grafo[1][2] = 1;
 	grafo[2][1] = 1;
 	grafo[0][2] = 1;
 	grafo[2][0] = 1;
 
-	if(tem_ciclo(grafo))
+	if(identificaCiclo(grafo))
 		cout << "O grafo tem ciclo!\n";
 	else
 		cout << "O grafo NAO tem ciclo!\n";
